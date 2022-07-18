d6051ec996d59979f91eb749a89194ee77e86367
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-18 19:20:36 -0400

    commit tree 810a5cc3f6d3eea4c97a65e57de78d16a3475acc

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params.include?('name')
+      name = params['name']
+      first = Student.where(first_name: name)
+      last = Student.where(last_name: name)
+      render json: last
+    else 
+      students = Student.all
+      render json: students
+    end
   end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 
 end
